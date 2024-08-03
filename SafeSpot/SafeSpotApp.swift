fab19104064a502ed9ba40fcd18000e626d17350
mchelliah1 <mchelliah01@gmail.com>
2024-08-03 16:30:08 -0700

    Initial Commit

---
+++ SafeSpot/SafeSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeSpotApp.swift
+//  SafeSpot
+//
+//  Created by Matthew Chelliah on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SafeSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
